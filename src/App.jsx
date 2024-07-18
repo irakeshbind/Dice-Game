7b3c89ae32fbdf0985156614c528b43750f4c52f
irakeshbind <rakeshbind351998@gmail.com>
2024-07-18 11:16:50 +0530

    Dice component  is a complete

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StartGame from './Component/StartGame'
+import './App.css';
+function App() {
+  return (
+    <StartGame/>
+  )
+}
+
+export default App
---
+++ src/Component/StartGame.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled  from "styled-components";
+const StartGame = () => {
+  return (
+    <Container>
+        <div>
+        <img src="/images/dices.jpg" alt="" />
+        </div>
+
+        <div className='content'>
+            <h1>Dice Game</h1>
+            <Button>Play now</Button>
+        </div>
+    </Container>
+  )
+}
+
+export default StartGame;
+
+const Container=styled.div`
+    max-width: 1180px;
+    display: flex;
+    margin: 0 auto;
+    align-items:center;
+    .content{
+        h1{
+            font-size: 96px;
+            white-space: nowrap;
+        }
+    }
+`;
+const Button =styled.button`
+color:white;
+padding:10px 18px;
+background:black;
+border-radius:5px;
+min-width:220px;
+border:none;
+font-size:16px ;
+border: 1px solid transparent;
+transition:  0.3s background ease-in;   
+cursor: pointer;
+&:hover{
+    background-color: white;
+    border: 1px solid black     ;
+    transition:  0.3s background ease-in;
+}
+`;
